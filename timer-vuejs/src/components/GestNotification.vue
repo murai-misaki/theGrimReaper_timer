c595304b768086f1e1a8c2f1c9479e291f3626a5
murai-misaki <f2030125@gmail.com>
2023-05-14 10:14:41 +0900

    add: ゲストログインユーザーの通知方法選択画面のコンポーネント #17

---
+++ timer-vuejs/src/components/GestNotification.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>30分ごとに立ち上がりのお知らせを致します。</h2>
+
+    <div class="explanation">
+      <span>‘’ 30分に一度3分、立って動く ‘’</span>
+      <p>一回に座ったまま過ごす時間が30分を超えないようにすること。これで座り過ぎのリスクはかなり減らせます。</p>
+    </div>
+
+    <div class="question">
+      <p>お知らせ方法はどちらをご希望でしょうか？</p>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  div {
+    text-align: center;
+  }
+  h2 {
+    background-color: #494949;
+    padding: 10px;
+    font-weight: normal;
+  }
+  span {
+    color: #CB0101;
+    font-size: 18px;
+  }
+  .explanation p {
+    padding-bottom: 20px;
+    border-bottom: 0.1px solid #D9D9D9;
+  }
+  .question p {
+    margin-top: 50px;
+  }
+</style>
