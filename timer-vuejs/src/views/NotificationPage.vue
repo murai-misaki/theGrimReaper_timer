c11702f0fc5a299500bdcdbc83779885fed4bbaf
murai-misaki <f2030125@gmail.com>
2023-05-14 22:36:34 +0900

    add: ボタンをクリックしたらモーダルウィンドウを表示させる #17

--- timer-vuejs/src/views/NotificationPage.vue
+++ timer-vuejs/src/views/NotificationPage.vue
@@ -1,15 +1,65 @@
 <template>
   <div class="container">
     <GestNotification />
+    <button @click="modal">Timer Start</button>
+    <HowtouseOKbuttonModal ref="howtouseOkbuttonModal" />
   </div>
 </template>
 
 <script>
-import GestNotification from '../components/GestNotification.vue'
-export default {
-  components: { GestNotification },
-}
+  import GestNotification from '../components/GestNotification.vue'
+  import HowtouseOKbuttonModal from '../components/HowtouseOKbuttonModal.vue'
+
+  export default {
+    components: { GestNotification, HowtouseOKbuttonModal },
+
+    methods: {
+      modal () {
+        this.$refs.howtouseOkbuttonModal.open()
+      }
+    }
+  }
 </script>
 
 <style scoped>
+  .container {
+    padding: 20px 70px 50px 70px;
+    border: 1px solid #D9D9D9;
+  }
+  
+  button {
+    width: 400px;
+    font-size: 19px;
+    background: #eee;
+    position: relative;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 240px;
+    padding: 20px 25px;
+    color: #313131;
+    transition: 0.3s ease-in-out;
+    font-weight: 500;
+    z-index:0;
+  }
+  button:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 0;
+    display: block;
+    background: #8F0B0B;
+    transition: .3s;
+    left:0;
+  }
+  button:hover {
+    color: #FFF;
+  }
+  button:hover:before {
+    width: 100%;
+    z-index: -1;
+  }
+
 </style>
