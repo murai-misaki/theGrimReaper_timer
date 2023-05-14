b84899c2fc063f4db7aa445658dd60d7f6099fb4
murai-misaki <f2030125@gmail.com>
2023-05-14 22:30:25 +0900

    Font Awesomeを使用してアイコンを表示 #17

--- timer-vuejs/src/main.js
+++ timer-vuejs/src/main.js
@@ -4,4 +4,11 @@ import router from './router'
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons"
+import { faEye } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faVolumeUp)
+library.add(faEye)
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
