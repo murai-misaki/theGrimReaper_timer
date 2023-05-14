2328bb95139d2b2bda3da6f6404549b498a047c9
murai-misaki <f2030125@gmail.com>
2023-05-14 11:04:30 +0900

    add: ラジオボタン #17

--- timer-vuejs/src/components/GestNotification.vue
+++ timer-vuejs/src/components/GestNotification.vue
@@ -10,10 +10,28 @@
     <div class="question">
       <p>お知らせ方法はどちらをご希望でしょうか？</p>
     </div>
+
+    <div class="selection">
+      <div class="sound-radio-button">
+        <input type="radio" id="sound" v-model="radio" value="false">
+        <label for="sound">サウンド通知</label>
+      </div>
+      <div class="push-radio-button">
+        <input type="radio" id="push" v-model="radio" value="true">
+        <label for="push">プッシュ通知</label>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  export default {
+  data () {
+    return {
+      radio: 'false'
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -35,5 +53,39 @@
   }
   .question p {
     margin-top: 50px;
+  }
+
+  input[type="radio"] {
+    width: 20px;
+    height: 20px;
+    border-radius: 100px;
+    position: relative;
+    vertical-align: sub;
+    margin-right: 5px;
+  }
+  input[type="radio"]::before, input[type="radio"]::after{
+    content: "";
+    display: block;
+    border-radius: 50%;
+    position: absolute;
+    transform: translateY(-50%);
+    top: 50%;
+  }
+  input[type="radio"]::before {
+    background-color: #D9D9D9;
+    border: 1px solid #494949;
+    height: 20px;
+    width: 20px;
+    left: 0px;
+  }
+  input[type="radio"]::after {
+    background-color: #494949;
+    opacity: 0;
+    height: 14px;
+    width: 14px;
+    left: 4px;
+  }
+  input[type="radio"]:checked::after {
+    opacity: 1;
   }
 </style>
