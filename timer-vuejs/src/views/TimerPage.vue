777963d95f5e5b0aa679e6a09d0b76765cc6c260
murai-misaki <f2030125@gmail.com>
2023-05-16 12:27:52 +0900

    add: カウントアップタイマー画面またはブレイクタイムタイマー画面を表示するページ #17

---
+++ timer-vuejs/src/views/TimerPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container welcome">
+    <p>Timer Page</p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+</style>
