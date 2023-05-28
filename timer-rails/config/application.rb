e6d1d34066ccfba4462b02c7d13394171639d0b5
murai-misaki <f2030125@gmail.com>
2023-05-28 11:58:49 +0900

    fix: デフォルトのタイムゾーンを日本時間にする #20

--- timer-rails/config/application.rb
+++ timer-rails/config/application.rb
@@ -33,5 +33,7 @@ module TimerRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.time_zone = 'Tokyo'
   end
 end
