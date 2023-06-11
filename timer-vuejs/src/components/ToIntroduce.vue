f50d05894c5788b121975128b5ca04f8cc0a3847
murai-misaki <f2030125@gmail.com>
2023-06-11 18:56:35 +0900

    アプリの機能紹介画像を実際のものに修正 #14

--- timer-vuejs/src/components/ToIntroduce.vue
+++ timer-vuejs/src/components/ToIntroduce.vue
@@ -24,14 +24,14 @@
   <div class="introduction">
     <h2>危機感を持つことで、30分に1回の立ち上がりを怠らないように促す機能</h2>
     <div class="body">
-      <img src="../assets/img/to_introduce3.png" alt="該当の機能画面" class="img-left" >
+      <img src="../assets/img/to_introduce1.png" alt="該当の機能画面" class="img-left" >
       <span class="number number-right">1</span>
       <p class="text-right">立ち上がらずに座り続けて1時間が経過すると、<br>座りすぎによって縮んだ寿命がリアルタイムで表示</p>
     </div>
     <div class="body">
       <span class="number number-left">2</span>
       <p class="text-left">1日ごとの、座っている時間と<br>1時間座り続けたことで縮んだ寿命を記録し、<br>直近1週間分をグラフ化</p>
-      <img src="../assets/img/to_introduce3.png" alt="該当の機能画面" class="img-right" >
+      <img src="../assets/img/to_introduce2.png" alt="該当の機能画面" class="img-right" >
     </div>
     <div class="body-last">
       <img src="../assets/img/to_introduce3.png" alt="該当の機能画面" class="img-left" >
@@ -42,7 +42,7 @@
   <div class="introduction">
     <h2>1日8時間以上座ることによって高まる罹患リスクや死亡リスク、<br>身体活動不足、水分不足も対策できる機能が備わっています</h2>
     <div class="body">
-      <img src="../assets/img/to_introduce3.png" alt="該当の機能画面" class="img-left" >
+      <img src="../assets/img/to_introduce4.png" alt="該当の機能画面" class="img-left" >
       <span class="number number-right">1</span>
       <p class="text-right">1日に8時間以上座り続けている場合に注意喚起<br>罹患リスクや死亡リスクが高まるのを防ぐ機能</p>
     </div>
@@ -58,12 +58,12 @@
         </div>
       </div>
       <div>
-        <img src="../assets/img/to_introduce3.png" alt="該当の機能画面" class="img-right5" >
+        <img src="../assets/img/to_introduce5.png" alt="該当の機能画面" class="img-right5" >
       </div>
     </div>
     <div class="body-last">
       <div>
-        <img src="../assets/img/to_introduce3.png" alt="該当の機能画面" class="img-left" >
+        <img src="../assets/img/to_introduce6.png" alt="該当の機能画面" class="img-left" >
       </div>
       <div>
         <div class="number7">
