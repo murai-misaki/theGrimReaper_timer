1f09fa9f3afbc5dc9369b213c030ebd032c1fc66
murai-misaki <f2030125@gmail.com>
2023-06-12 18:05:18 +0900

    add: SingupRecommend.vue・ChangeToLoginuser.vue・FooterLink.vue #28

--- timer-vuejs/src/views/GuestloginPage.vue
+++ timer-vuejs/src/views/GuestloginPage.vue
@@ -1,17 +1,38 @@
 <template>
-  <h1>GestloginPage.vue</h1>
+  <div v-show="show">
+    <SingupRecommend @showChangeToLoginuser="showChangeToLoginuser" />
+  </div>
+  <div v-show="!show">
+    <ChangeToLoginuser />
+  </div>
+  <div class="footer">
+    <FooterLink />
+  </div>
 </template>
 
 <script>
+  import SingupRecommend from '../components/SingupRecommend.vue'
+  import ChangeToLoginuser from '../components/ChangeToLoginuser.vue'
+  import FooterLink from '../components/FooterLink.vue'
+
   export default {
+    components: { SingupRecommend, ChangeToLoginuser, FooterLink },
+
     data () {
       return {
+        show: true
       }
     },
     methods: {
+      showChangeToLoginuser () {
+        this.show = false
+      }
     }
   }
 </script>
 
 <style scoped>
+  .footer {
+    margin-top: 80px;
+  }
 </style>
