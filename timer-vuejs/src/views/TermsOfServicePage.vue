f56de3b9b842b92754ba23b3e557183b6f56dedf
murai-misaki <f2030125@gmail.com>
2023-07-14 18:34:14 +0900

    add: 利用規約ページ #22

---
+++ timer-vuejs/src/views/TermsOfServicePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <TermsOfService />
+  <FooterLink />
+</template>
+
+<script>
+  import TermsOfService from '../components/TermsOfService.vue'
+  import FooterLink from '../components/FooterLink.vue'
+
+  export default {
+    components: { TermsOfService, FooterLink },
+
+    data () {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+</style>
