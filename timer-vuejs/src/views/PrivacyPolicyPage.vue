5f6b3c3e79ec291521c9edae052e86433dd0e1a9
murai-misaki <f2030125@gmail.com>
2023-07-14 18:36:34 +0900

    add: プライバシーポリシーページ #22

---
+++ timer-vuejs/src/views/PrivacyPolicyPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <PrivacyPolicy />
+  <FooterLink />
+</template>
+
+<script>
+  import PrivacyPolicy from '../components/PrivacyPolicy.vue'
+  import FooterLink from '../components/FooterLink.vue'
+
+  export default {
+    components: { PrivacyPolicy, FooterLink },
+
+    data () {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+</style>
