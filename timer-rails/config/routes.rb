430a7be02bd072ee73939a60b790cf71fed08881
murai-misaki <f2030125@gmail.com>
2023-07-20 18:39:44 +0900

    add: deviseのパスワードリセットメールの件名とエラーメッセージの日本語化・開発環境で送信したメールをブラウザで確認できるように #19

--- timer-rails/config/routes.rb
+++ timer-rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   constraints format: :json do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: 'auth/registrations'
