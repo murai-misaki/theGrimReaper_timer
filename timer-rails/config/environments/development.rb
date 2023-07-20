a114beb740daca4ad77d240b87f72eaf42750bfd
murai-misaki <f2030125@gmail.com>
2023-07-21 00:27:47 +0900

    add: configの導入 #19

--- timer-rails/config/environments/development.rb
+++ timer-rails/config/environments/development.rb
@@ -33,9 +33,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -52,4 +49,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 end
--- timer-rails/config/environments/production.rb
+++ timer-rails/config/environments/production.rb
@@ -102,4 +102,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 end
