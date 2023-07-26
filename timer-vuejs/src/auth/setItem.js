b0f0fff3a7624125fa22cf324cca3fc43d051a1f
murai-misaki <f2030125@gmail.com>
2023-07-26 10:11:15 +0900

    add: プロフィール編集で必要なユーザー名をローカルストレージに保存 #26

--- timer-vuejs/src/auth/setItem.js
+++ timer-vuejs/src/auth/setItem.js
@@ -1,8 +1,9 @@
-const setItem = (headers, guest) => {
+const setItem = (headers, guest, name) => {
   window.localStorage.setItem('access-token', headers['access-token'])
   window.localStorage.setItem('client', headers.client)
   window.localStorage.setItem('uid', headers.uid)
   window.localStorage.setItem('guest', guest)
+  window.localStorage.setItem('name', name)
 }
 
 export default setItem
