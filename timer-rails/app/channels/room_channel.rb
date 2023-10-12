45a86dc6804629f79a25cf5525b4212c7e1f0a28
murai-misaki <f2030125@gmail.com>
2023-10-12 23:55:02 +0900

    add: リアルタイムチャット機能 #83

---
+++ timer-rails/app/channels/room_channel.rb
@@ -0,0 +1,15 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'room_channel'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data)
+    user = User.find_by(email: data['email'])
+    
+    Message.create_and_send(data, user)
+  end
+end
